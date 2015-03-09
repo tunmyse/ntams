9016fa1da01ab510c6eff6d5ea4bf392d8050043
tunmyse <akinsolatunmise@gmail.com>
2015-03-09 13:21:16 +0100

    moved installation related files into project root folder

---
+++ install_files/language/english/installer_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['account_not_created'] = 'Installation Error! Accounts could not be created.';
+$lang['database_not_created'] = 'Installation Error! Database tables could not be created.';
+
+/* End of file installer_lang.php */
+/* Location: ./base/language/english/installer_lang.php */
